80c60c103e13959d15145d9790e8cd417c550cbf
NamasteCS <963ajp@gmail.com>
2025-05-22 13:42:27 +0530

    Update get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -201,7 +201,7 @@ async def get_msg(userbot, sender, edit_id, msg_link, i, message):
             if chat in saved_channel_ids:
                 await app.edit_message_text(
                     message.chat.id, edit_id,
-                    "Sorry! This channel is protected by **__Team SPY__**."
+                    "Sorry! This channel is protected by **II_LevelUP_II**."
                 )
                 return
             
@@ -563,7 +563,7 @@ user_caption_preferences = {}
 async def set_rename_command(user_id, custom_rename_tag):
     user_rename_preferences[str(user_id)] = custom_rename_tag
 
-get_user_rename_preference = lambda user_id: user_rename_preferences.get(str(user_id), 'Team SPY')
+get_user_rename_preference = lambda user_id: user_rename_preferences.get(str(user_id), '@II_LevelUP_II')
 
 async def set_caption_command(user_id, custom_caption):
     user_caption_preferences[str(user_id)] = custom_caption
